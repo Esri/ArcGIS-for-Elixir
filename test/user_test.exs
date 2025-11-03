337b7ba2a2050a4c66524fd9b5fd21a48d74af72
Aaron Seigo <aseigo@esri.com>
2025-11-03 16:45:17 +0100

    assert the request path when fetching a user

--- test/user_test.exs
+++ test/user_test.exs
@@ -4,6 +4,7 @@ defmodule ArcGIS.Test.User do
 
   test "Translates a map to a user struct" do
     Req.Test.stub(ArcGIS, fn conn ->
+      assert(conn.request_path == "/sharing/rest/community/self")
       Req.Test.json(conn, Fixtures.Network.json("user"))
     end)
 
