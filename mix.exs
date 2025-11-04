71bb02ef53f3955ba64bac7d6004b5b8552797d1
Aaron Seigo <aseigo@esri.com>
2025-11-04 16:49:39 +0100

    add credo for src checking

--- mix.exs
+++ mix.exs
@@ -32,9 +32,12 @@ defmodule ArcGIS.MixProject do
   defp deps do
     [
       {:cachex, "~> 4.0"},
+      {:inflex, "~> 2.0"},
       {:req, "~> 0.5"},
       {:telemetry, "~> 1.0"},
-      {:inflex, "~> 2.0"},
+
+      # dev depencencies
+      {:credo, "~> 1.0"},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:plug, "~> 1.0", only: [:test]}
