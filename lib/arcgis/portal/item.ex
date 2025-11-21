aa364f4af705f9a04c5fd20ba5180f99c9fbf33a
Aaron Seigo <aseigo@esri.com>
2025-11-21 14:48:21 +0100

    Item struct and converter

--- lib/arcgis/portal/item.ex
+++ lib/arcgis/portal/item.ex
@@ -1,6 +1,81 @@
 defmodule ArcGIS.Portal.Item do
   alias ArcGIS.Portal
 
+  # not include:
+  #   advancedSettings: undocumented
+  # apiToken[12]ExpirationDate: undocumented
+  # appCategories: undocumented
+  # avgRating: undocumented
+  # banner: undocumented
+  # classification: undocumented
+  # groupDesignations: undocumented
+  # guid: undocumented
+  # industries: undocumented
+  # languages: undocumented
+  # largeThumbnail: is this used?
+  # listed: undocumented
+  # subInfo: undocumented
+  # scoreCompleteness: undocumented
+  # proxyFilter: undocumented
+
+  defstruct [
+    :id,
+    :access,
+    :accessInformation,
+    :categories,
+    :culture,
+    :description,
+    :documentation,
+    :extent,
+    :licenseInfo,
+    :name,
+    :counts,
+    :owner,
+    :properties,
+    :proxyFilter,
+    :scoreCompleteness,
+    :screenshots,
+    :size,
+    :snippet,
+    :spatial_reference,
+    :tags,
+    :thumbnail,
+    :timestamps,
+    :title,
+    :type,
+    :type_keywords,
+    :url
+  ]
+
+  @type access :: :private | :shared | :org | :public
+  @type counts :: %{comments: non_neg_integer, ratings: non_neg_integer, views: non_neg_integer}
+  @type t :: %__MODULE__{
+          id: Portal.id(),
+          access: access,
+          accessInformation: String.t(),
+          categories: [String.t()],
+          counts: counts,
+          culture: String.t(),
+          description: String.t(),
+          documentation: String.t(),
+          extent: ArcGIS.Extent.t(),
+          licenseInfo: String.t(),
+          name: String.t(),
+          owner: String.t(),
+          properties: map,
+          screenshots: [String.t()],
+          size: integer,
+          snippet: String.t(),
+          spatial_reference: ArcGIS.SpatialReference.t(),
+          tags: [String.t()],
+          thumbnail: String.t(),
+          timestamps: ArcGIS.Timestamps.t(),
+          title: String.t(),
+          type: String.t(),
+          type_keywords: [String.t()],
+          url: String.t()
+        }
+
   @type portal_item_search_terms :: %{
           search_text: String.t(),
           keywords: [{key :: String.t(), value :: String.t()}],
@@ -23,7 +98,7 @@ defmodule ArcGIS.Portal.Item do
       options
       |> Keyword.put(:is_features_query?, false)
       |> Keyword.put(:params, %{q: query})
-      |> Keyword.put(:transform, &to_struct/1)
+      |> Keyword.put(:transform, &new/1)
 
     Portal.get(portal, "/search", all_options)
   end
@@ -34,8 +109,63 @@ defmodule ArcGIS.Portal.Item do
     Portal.get(portal, "/content/items/#{id}", options)
   end
 
-  def to_struct(arcgis_map) do
-    arcgis_map
+  @spec new(map) :: t()
+  def new(%{} = arcgis_map) do
+    spatial_reference = %ArcGIS.SpatialReference{
+      wkid: from_map(arcgis_map, "spatialReference", 0)
+    }
+
+    %__MODULE__{
+      id: from_map(arcgis_map, "id", ""),
+      access: String.to_existing_atom(from_map(arcgis_map, "access", "private")),
+      accessInformation: from_map(arcgis_map, "accessInformation", ""),
+      categories: from_map(arcgis_map, "categories", []),
+      counts: counts_from_map(arcgis_map),
+      culture: from_map(arcgis_map, "culture", []),
+      description: from_map(arcgis_map, "description", ""),
+      documentation: from_map(arcgis_map, "documentation", ""),
+      extent: ArcGIS.Extent.new(Map.get(arcgis_map, "extent")),
+      licenseInfo: from_map(arcgis_map, "", ""),
+      name: from_map(arcgis_map, "name", ""),
+      owner: from_map(arcgis_map, "owner", ""),
+      properties: from_map(arcgis_map, "properties", %{}),
+      screenshots: [from_map(arcgis_map, "screenshots", [])],
+      size: from_map(arcgis_map, "size", 0),
+      snippet: from_map(arcgis_map, "snippet", ""),
+      spatial_reference: spatial_reference,
+      tags: [from_map(arcgis_map, "tags", [])],
+      thumbnail: from_map(arcgis_map, "thumbnail", ""),
+      timestamps: timestamps_from_map(arcgis_map),
+      title: from_map(arcgis_map, "title", ""),
+      type: from_map(arcgis_map, "type", ""),
+      type_keywords: [from_map(arcgis_map, "typeKeywords", "")],
+      url: from_map(arcgis_map, "url", "")
+    }
+  end
+
+  defp from_map(map, key, default) do
+    # || is used here, as if the map has the entry but it is
+    # explicitly set to nil, Map.get/3 will return the nil value
+    # rather than the default
+    Map.get(map, key) || default
+  end
+
+  @spec counts_from_map(map) :: counts
+  defp counts_from_map(map) do
+    %{
+      comments: from_map(map, "numComments", 0),
+      ratings: from_map(map, "numRatings", 0),
+      views: from_map(map, "numViews", 0)
+    }
+  end
+
+  @spec timestamps_from_map(map) :: ArcGIS.Timestamps.t()
+  defp timestamps_from_map(map) do
+    %ArcGIS.Timestamps{
+      created: from_map(map, "created", 0),
+      last_access: from_map(map, "lastViewed", 0),
+      modified: from_map(map, "modified", 0)
+    }
   end
 
   defp add_portal_item_search_keywords(acc, search_terms) do
