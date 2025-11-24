695a79dadf49cb032cc9259b1e32c23f1b3861e3
Aaron Seigo <aseigo@esri.com>
2025-11-24 14:27:47 +0100

    docs and a way to fetch the "best" srid for an ArcGIS.SpatialReference

--- lib/arcgis/spatial_reference.ex
+++ lib/arcgis/spatial_reference.ex
@@ -7,6 +7,8 @@ defmodule ArcGIS.SpatialReference do
   """
   @type t :: %{wkid: non_neg_integer, alternatives: [non_neg_integer]}
 
+  @spec from_map(source :: map) :: t()
+  @doc "Creates a `%ArcGIS.SpatialReference{}` from ArcGIS response data"
   def from_map(%{"latestWkid" => latest, "wkid" => wkid}) do
     %__MODULE__{wkid: latest, alternatives: [wkid]}
   end
@@ -16,4 +18,9 @@ defmodule ArcGIS.SpatialReference do
   end
 
   def from_map(_), do: %__MODULE__{}
+
+  @spec best_srid(t()) :: non_neg_integer
+  @doc "Given a spatial reference, return the 'best' spatial reference id to use."
+  def best_srid(%{alternatives: [alt | _]}), do: alt
+  def best_srid(%{wkid: wkid}), do: wkid
 end
