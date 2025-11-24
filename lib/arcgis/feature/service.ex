2f6ec904eff04f0ef713f3d34fed130a10a11999
Aaron Seigo <aseigo@esri.com>
2025-11-24 14:21:34 +0100

    create local options to avoid accidental option polution

--- lib/arcgis/feature/service.ex
+++ lib/arcgis/feature/service.ex
@@ -118,7 +118,13 @@ defmodule ArcGIS.Feature.Service do
 
   @spec fetch_and_cache_url(t(), options :: Keyword.t()) :: {:ok, String.t()} | {:error, term}
   defp fetch_and_cache_url(service, options) do
-    case Portal.get(service.portal, "/content/items", Keyword.put(options, :selector, ["url"])) do
+    options = [selector: ["url"], auth_token: Keyword.get(options, :auth_token, "")]
+
+    case Portal.get(
+           service.portal,
+           "/content/items/#{service.id}",
+           options
+         ) do
       {:ok, url} ->
         result = cache(service, url)
         {:ok, result}
