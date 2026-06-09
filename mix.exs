6e1f973e9fd4b80553ccc19fb369197899e993b1
Aaron Seigo <aseigo@esri.com>
2026-06-09 09:53:19 +0200

    update deps, make the plug dep explicit

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule ArcGIS.MixProject do
     [
       {:cachex, "~> 4.0"},
       {:inflex, "~> 2.0"},
+      {:plug, "~> 1.0"},
       {:req, "~> 0.5"},
       {:telemetry, "~> 1.0"},
       {:geometry, "~> 1.0"},
