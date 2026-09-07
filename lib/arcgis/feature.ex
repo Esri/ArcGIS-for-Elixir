44e6c3560ca930747260e47db7ddc60288e80473
Aaron Seigo <aseigo@esri.com>
2026-09-07 14:51:52 +0200

    schema awareness, wrap and unwrap global IDs

--- lib/arcgis/feature.ex
+++ lib/arcgis/feature.ex
@@ -12,12 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
- defmodule ArcGIS.Feature do
+defmodule ArcGIS.Feature do
   @moduledoc """
   Access to features in an ArcGIS feature service.
   """
 
-  alias ArcGIS.Feature.Schema
   alias ArcGIS.Feature.Service
   alias ArcGIS.Telemetry
 
@@ -56,12 +55,15 @@
 
   def from_map(data, _metadata), do: data
 
-  @spec query(Service.t(), layer_id :: non_neg_integer(), [Portal.request_option()]) :: [t()]
+  @spec query(Service.t(), layer_id :: non_neg_integer(), [ArGIS.Portal.request_option()]) ::
+          Portal.ResultSet.t()
   @doc "Query features in a Feature Service layer or table"
   def query(%Service{} = feature_service, layer_id, options \\ [])
       when is_number(layer_id) and layer_id >= 0 do
     options = Keyword.put(options, :transform, &__MODULE__.from_map/2)
+
     Service.post(feature_service, "/#{layer_id}/query", [], options)
+    |> normalize_results(feature_service, layer_id, options)
   end
 
   @spec mutate(
@@ -71,14 +73,16 @@
         ) :: boolean
   @doc "Add, update, and/or delete features from one or more layers. Defaults to rolling back on failure."
   def mutate(service, mutations, options \\ []) do
+    service = Service.with_schema(service, options)
+
     document =
       Enum.map(
         mutations,
         fn {layer_id, edits} ->
           %{id: layer_id}
-          |> add_create(edits)
-          |> add_update(edits)
-          |> add_delete(edits)
+          |> add_create(edits, service)
+          |> add_update(edits, service)
+          |> add_delete(edits, service)
         end
       )
       |> :json.encode()
@@ -97,58 +101,79 @@
         metadata: %{service: service, action: :mutate_features}
       })
 
-    # TODO: properly support PBF formats
+    # TODO: support PBF formats
     document = [edits: document]
 
     Service.post(service, "/applyEdits", document, options_with_params)
   end
 
-  @spec sanitize(features :: [t()], schema :: Schema.t() | nil) :: [t()]
+  @spec sanitize(features :: [t()], layer_id :: non_neg_integer, service :: Service.t()) :: [t()]
   @doc "Conforms a list of feature to ArcGIS requirements, making them appropriate for e.g. use in mutations"
-  def sanitize(features, schema \\ nil) do
-    # TODO: pass in the schema it should adhere to
-    Enum.map(features, fn feature -> sanitize_feature(feature, schema) end)
+  def sanitize(features, _layer_id, %Service{schema: nil}) do
+    features
   end
 
-  defp add_create(arcgis_mutation, mutations) do
+  def sanitize(features, layer_id, %Service{schema: schema, portal: %{type: portal_type}}) do
+    # TODO: domain support; add `domain` field to field_types
+    field_types =
+      Enum.find_value(
+        schema,
+        fn
+          {_, %{id: ^layer_id, fields: fields}} -> fields
+          _ -> false
+        end
+      )
+      |> Enum.reduce(%{}, fn field, acc -> Map.put(acc, field.name, field.type) end)
+
+    Enum.map(features, fn feature -> sanitize_feature(feature, field_types, portal_type) end)
+  end
+
+  defp sanitize_feature(%{"attributes" => attributes} = feature, field_types, portal_type) do
+    %{feature | "attributes" => sanitize_attributes(attributes, field_types, portal_type)}
+  end
+
+  defp sanitize_attributes(attributes, field_types, portal_type) do
+    # TODO: schema adherence
+    Enum.reduce(attributes, %{}, fn {key, value}, acc ->
+      sanitize_attribute(key, value, field_types, portal_type, acc)
+    end)
+  end
+
+  defp sanitize_attribute(key, value, field_types, portal_type, acc) when is_binary(key) do
+    # TODO: check other types, e.g. dates, etc?
+    value =
+      case Map.get(field_types, key) do
+        "esriFieldTypeGlobalID" -> wrap_global_id(value, portal_type)
+        _ -> value
+      end
+
+    Map.put(acc, String.downcase(key), value)
+  end
+
+  defp sanitize_attribute(key, value, _field_typess, _portal_type, acc) do
+    Map.put(acc, key, value)
+  end
+
+  defp add_create(%{id: layer_id} = arcgis_mutation, mutations, service) do
     features =
       mutations
       |> Map.get(:create, [])
-      |> sanitize()
+      |> sanitize(layer_id, service)
 
     Map.put(arcgis_mutation, :adds, features)
   end
 
-  defp add_update(arcgis_mutation, mutations) do
+  defp add_update(%{id: layer_id} = arcgis_mutation, mutations, service) do
     features =
       mutations
       |> Map.get(:updates, [])
-      |> sanitize()
+      |> sanitize(layer_id, service)
 
     Map.put(arcgis_mutation, :updates, features)
   end
 
-  defp add_delete(arcgis_mutation, mutations) do
+  defp add_delete(arcgis_mutation, mutations, _service) do
     Map.put(arcgis_mutation, :deletes, Map.get(mutations, :delete, []))
-  end
-
-  defp sanitize_feature(%{"attributes" => attributes} = feature, schema) do
-    %{feature | "attributes" => sanitize_attributes(attributes, schema)}
-  end
-
-  defp sanitize_attributes(attributes, _schema) do
-    # TODO: schema adherence
-    # TODO: domain support
-    # TODO: global ID brace wrapping
-    Enum.reduce(attributes, %{}, &sanitize_attribute/2)
-  end
-
-  defp sanitize_attribute({key, value}, acc) when is_binary(key) do
-    Map.put(acc, String.downcase(key), value)
-  end
-
-  defp sanitize_attribute({key, value}, acc) do
-    Map.put(acc, key, value)
   end
 
   defp as_geometry(Geometry.Polygon, geometry, srid) do
@@ -213,5 +238,94 @@
 
   defp as_geometry(Geometry.Point, %{"x" => x, "y" => y}, srid) do
     Geometry.Point.new(x, y, srid)
+  end
+
+  defp normalize_results(
+         {:ok, %ArcGIS.Portal.ResultSet{} = result_set},
+         feature_service,
+         layer_id,
+         options
+       ) do
+    {:ok,
+     %{
+       result_set
+       | results: normalize_features(result_set.results, feature_service, layer_id, options)
+     }}
+  end
+
+  defp normalize_results({:ok, _} = results, _feature_service, _layer_id, _options) do
+    results
+  end
+
+  defp normalize_results(error, _feature_service, _layer_id, _options) do
+    error
+  end
+
+  defp normalize_features(
+         features,
+         %Service{portal: %ArcGIS.Portal{type: :online}},
+         _layer_id,
+         _options
+       ) do
+    # ArcGIS Online provides results exactly as we would expect them,
+    # so do nothing to the features
+    features
+  end
+
+  defp normalize_features(features, %Service{schema: %{} = schema}, layer_id, _options) do
+    global_id_fields = global_id_fields(schema, layer_id)
+    Enum.map(features, fn feature -> normalize_feature(feature, global_id_fields) end)
+  end
+
+  defp normalize_features(features, service, layer_id, options) do
+    # no schema, try to fetch one
+    global_id_fields =
+      case ArcGIS.Feature.Schema.get(service, options) do
+        {:ok, schema} -> global_id_fields(schema, layer_id)
+        _ -> ["GlobalID"]
+      end
+
+    Enum.map(features, fn feature -> normalize_feature(feature, global_id_fields) end)
+  end
+
+  defp normalize_feature(%{"attributes" => attrs} = feature, fields) do
+    normalized_attrs =
+      Enum.reduce(
+        fields,
+        attrs,
+        fn field, attrs ->
+          Map.replace_lazy(attrs, field, &unwrap_global_id/1)
+        end
+      )
+
+    %{feature | "attributes" => normalized_attrs}
+  end
+
+  defp unwrap_global_id("{" <> rest) do
+    String.slice(rest, 0, String.length(rest) - 1)
+  end
+
+  defp unwrap_global_id(id), do: id
+
+  defp wrap_global_id(id, :online), do: id
+  defp wrap_global_id("{" <> _ = id, _portal_type), do: id
+  defp wrap_global_id(id, _portal_type), do: "{#{id}}"
+
+  defp global_id_fields(schema, layer_id) do
+    # extract the fields for the layer
+    fields =
+      Enum.find_value(
+        schema,
+        fn
+          {_, %{id: ^layer_id, fields: fields}} -> fields
+          _ -> false
+        end
+      )
+
+    # return only global ID fields
+    Enum.reduce(fields || [], [], fn
+      %ArcGIS.Feature.Schema.Field{name: name, type: "esriFieldTypeGlobalID"}, acc -> [name | acc]
+      _, acc -> acc
+    end)
   end
 end
