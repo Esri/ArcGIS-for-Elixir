de623507ddb1ba1eb226a6bf870d4b07d7566722
Aaron Seigo <aseigo@esri.com>
2026-09-10 12:36:24 +0200

    2025->2026

--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -11,7 +11,7 @@ defmodule ArcGIS.Test.Fixtures do
   def portal(:discovered) do
     portal(:base)
     |> Map.put(:type, :online)
-    |> Map.put(:version, {2025, 3})
+    |> Map.put(:version, {2026, 3})
     |> Map.put(:help_url, URI.new!("https://doc.arcgis.com/en/arcgis-online/"))
   end
 
